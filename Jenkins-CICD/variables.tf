a40a036a64b0bc1160f8864ebface724a5bb6d25
bhavnith <155289027+bhavnith@users.noreply.github.com>
2023-12-31 21:10:22 +0530

    Update variables.tf

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -4,26 +4,26 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "Tetris"
 }
 variable "instance_type" {
   description = "instance type for ec2"
-  default     = "t2.medium"
+  default     = "t2.large"
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-04f5097681773b989"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "purplehaze777"
+  default     = "eksbucket"
 }
 
 variable "dynamodb_table" {
   description = "The name of the dynamodb table"
   type        = string
-  default     = "purplehaze777-dynamodb-table"
+  default     = "eks-dynamodb-table"
 }
 
 variable "acl" {
